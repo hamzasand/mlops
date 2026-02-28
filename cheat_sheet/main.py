450d3b2c482a2b2cb93661fa8cf1ef3dcc6eb870
hamzasand <mhamzasaeed762@gmail.com>
2026-02-28 21:25:22 +0500

    compelet up to modules

---
+++ cheat_sheet/main.py
@@ -0,0 +1,15 @@
+# import calculator
+# sum = calculator.add(1,3)
+# print(sum)
+# import math as m
+
+# print(m.sqrt(4))
+
+# import random
+# print(random.randint(50,100))
+
+
+from utils.string_tools import upper
+
+print(upper("python"))
+
